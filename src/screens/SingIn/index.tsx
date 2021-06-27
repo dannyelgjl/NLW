136de3eca2b61ff3a17d5dd70d0d3990fd09f380
dannyelgjl <dannyelgjl@gmail.com>
2021-06-27 15:30:19 -0300

    feat: adding image to home

--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Container, Title, Input } from './styles';
+import { Container, Title, Input, ImageHome } from './styles';
 
 const SingIn: React.FC = () => {
   const [text, setText] = useState('');
 
   return (
     <Container>
+      <ImageHome />
+
       <Title>
         Daniel Gomes Jeronimo Leite
       </Title>
--- src/screens/SingIn/styles.ts
+++ src/screens/SingIn/styles.ts
@@ -1,7 +1,16 @@
 import styled from 'styled-components/native';
+import image from '../../assets/illustration.png';
 
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ImageHome = styled.Image.attrs({
+  source: image,
+})``;
 
 export const Title = styled.Text``;
 
